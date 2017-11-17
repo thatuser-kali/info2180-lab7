1f90a62fcf2dd0da440ebd6926c07c0c8ebe2a51
thatuser-kali <thatuser-kali@users.noreply.github.com>
2017-11-17 06:05:32 -0600

    Initial

--- world.php
+++ world.php
@@ -8,43 +8,40 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
 
-if (!empty($_GET)) {
+if(!isset($_GET)){
+        
+        if(array_key_exists("all", $_GET) && $_GET['all'] === 'true'){
+            $stmt = $conn->query("SELECT * FROM countries");
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    if (array_key_exists("all", $_GET) && $_GET['all'] === 'true'){
-        $stmt = $conn->query("SELECT * FROM countries");
-    
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                echo '<div class="modal-content"> <span class="off">&times;</span><h1>All Results</h1>';
+                    
+            
+                echo '<ul>';
+                foreach ($results as $row) {
+                  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+                }
+                echo '</ul>';
         
-        echo '<div class="modal-content">
-    <span class="close">&times;</span><h1>Search Results</h1>';
-    
-        echo '<ul>';
-        foreach ($results as $row) {
-          echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
         }
-        echo '</ul>';
-        echo '</div>';
+        
+        else if(array_key_exists("country", $_GET)){
+            $country = $_GET['country'];
+            $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%' ");
+
+                echo '<div class="modal-content"><span class="off">&times;</span><h1>Search Results</h1>';
+
+                echo '<ul>';
+                foreach ($stmt as $row) {
+                  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+                }
+                echo '</ul>';
+            }
         
     }
-    else if (array_key_exists("country", $_GET)){
-        $country = '"' . $_GET['country'] . '"';
-        $stmt = $conn->query("SELECT * FROM countries WHERE name = $country");
+
+    do{
         
-        echo '<div class="modal-content">
-    <span class="close">&times;</span><h1>Search Results</h1>
-    ';
-    
-        $data =  '<ul>';
-        foreach ($stmt as $row) {
-          $data.= '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
-        }
-        $data .= '</ul>';
-        $data.= '</div>';
-        echo $data;
-    }
-    
-}else{
-    $data = '<div class="modal-content">
-    <span class="close">&times;</span> No country was requested </div>';
-    echo $data;
-}
+        echo '<div class="modal-content"> <span class="off">&times;</span> No country was requested </div>';
+         
+    } while (empty($_GET));
